2d0b99bd4c370f3f1b93f30ba9d3790cdca7fd23
dperkins3pi <dperkins3pi@gmail.com>
2024-10-29 15:09:40 -0600

    Finished prob 2

--- Parallel_Intro/parallel_intro.py
+++ Parallel_Intro/parallel_intro.py
@@ -147,8 +147,6 @@ def parallel_trapezoid_rule(f, a, b, n=200):
 if __name__=="__main__":
     # Problem 1
     # prob1()
-    # plt.plot()
-    # plt.show()
     
     # Problem 2
     # means, mins, maxs = prob2()
@@ -157,7 +155,7 @@ if __name__=="__main__":
     # print(maxs)
     
     # Prob 3
-    prob3() # PLOTTING NOT WORKING ANYMORE!!!!!!!!!!!!
+    # prob3() 
     
     # Prob 4
     # def f(x): return 3*x**2 - 2*np.sin(x) + 5.3
